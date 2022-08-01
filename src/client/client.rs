262359e0bc556e05fa6d82962dc20edabad4a005
Matteo Monti <matteo.monti@epfl.ch>
2022-08-01 12:36:02 +0000

    Begin implementation of `Client`

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,39 +1,140 @@
 use crate::broadcast::Message;
 
-use talk::{crypto::KeyChain, sync::fuse::Fuse};
+use doomstack::{here, Doom, ResultExt, Top};
 
-use tokio::sync::mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender};
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex as StdMutex},
+    time::Duration,
+};
+
+use talk::{
+    crypto::KeyChain,
+    net::{DatagramDispatcher, DatagramDispatcherSettings},
+    sync::fuse::Fuse,
+};
+
+use tokio::{
+    io,
+    net::{self, ToSocketAddrs},
+    sync::{
+        mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender},
+        Mutex as TokioMutex,
+    },
+    time,
+};
 
 type BroadcastInlet = MpscSender<Message>;
 type BroadcastOutlet = MpscReceiver<Message>;
 
 pub struct Client {
-    broadcast_inlet: BroadcastInlet,
+    brokers: Arc<StdMutex<Vec<SocketAddr>>>,
+    broadcast_inlet: TokioMutex<BroadcastInlet>,
     _fuse: Fuse,
+}
+
+#[derive(Doom)]
+pub enum ClientError {
+    #[doom(description("Failed to resolve host: {:?}", source))]
+    #[doom(wrap(resolve_failed))]
+    ResolveFailed { source: io::Error },
+    #[doom(description("Host unknown"))]
+    HostUnknown,
 }
 
 impl Client {
     pub fn new(id: u64, keychain: KeyChain) -> Self {
+        let brokers = Arc::new(StdMutex::new(Vec::new()));
+
         let (broadcast_inlet, broadcast_outlet) = mpsc::channel(1); // Only one broadcast is performed at a time
+        let broadcast_inlet = TokioMutex::new(broadcast_inlet);
 
         let fuse = Fuse::new();
 
-        fuse.spawn(async move {
-            let _ = Client::run(id, keychain, broadcast_outlet).await;
-        });
+        {
+            let brokers = brokers.clone();
+            fuse.spawn(Client::run(id, keychain, brokers, broadcast_outlet));
+        }
 
         Client {
+            brokers,
             broadcast_inlet,
             _fuse: fuse,
         }
     }
 
-    pub async fn broadcast(&mut self, message: Message) {
+    pub async fn add_broker<A>(&self, broker: A) -> Result<(), Top<ClientError>>
+    where
+        A: ToSocketAddrs,
+    {
+        let mut addresses = net::lookup_host(broker)
+            .await
+            .map_err(ClientError::resolve_failed)
+            .map_err(ClientError::into_top)
+            .spot(here!())?;
+
+        let address = addresses
+            .next()
+            .ok_or(ClientError::HostUnknown.into_top())
+            .spot(here!())?;
+
+        self.brokers.lock().unwrap().push(address);
+
+        Ok(())
+    }
+
+    pub async fn broadcast(&self, message: Message) {
+        let broadcast_inlet = self.broadcast_inlet.lock().await;
+
         // If the `Client` still exists, then `_fuse` has not
         // dropped, `run` is still running and `broadcast_outlet`
         // still exists
-        self.broadcast_inlet.send(message).await.unwrap();
+        broadcast_inlet.send(message).await.unwrap();
     }
 
-    async fn run(_id: u64, _keychain: KeyChain, _broadcast_outlet: BroadcastOutlet) {}
+    async fn run(
+        _id: u64,
+        _keychain: KeyChain,
+        brokers: Arc<StdMutex<Vec<SocketAddr>>>,
+        mut broadcast_outlet: BroadcastOutlet,
+    ) {
+        let dispatcher = DatagramDispatcher::bind(
+            "0.0.0.0:0",
+            DatagramDispatcherSettings {
+                workers: 1,
+                ..Default::default()
+            },
+        )
+        .await
+        .unwrap(); // TODO: Determine if this error should be handled
+
+        let (sender, receiver) = dispatcher.split();
+        let sender = Arc::new(sender);
+
+        loop {
+            // Wait for the next message to broadcast
+
+            let message = match broadcast_outlet.recv().await {
+                Some(message) => message,
+                None => return, // `Client` has dropped, shutdown
+            };
+
+            let fuse = Fuse::new();
+
+            let brokers = brokers.clone();
+            let sender = sender.clone();
+
+            fuse.spawn(async move {
+                for index in 0.. {
+                    let broker = loop {
+                        if let Some(broker) = brokers.lock().unwrap().get(index).cloned() {
+                            break broker;
+                        }
+
+                        time::sleep(Duration::from_secs(1)).await;
+                    };
+                }
+            });
+        }
+    }
 }
