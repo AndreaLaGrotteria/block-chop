e70d31846d19484067bee89d3ba8f269b6c23b74
Matteo Monti <matteo.monti@epfl.ch>
2022-08-01 15:44:24 +0000

    Refactor `Client` to a multi-file module

---
+++ src/client/client/mod.rs
@@ -0,0 +1,89 @@
+use crate::broadcast::Message;
+
+use doomstack::{here, Doom, ResultExt, Top};
+
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex as StdMutex},
+};
+
+use talk::{crypto::KeyChain, sync::fuse::Fuse};
+
+use tokio::{
+    io,
+    net::{self, ToSocketAddrs},
+    sync::{
+        mpsc::{self, Sender as MpscSender},
+        Mutex as TokioMutex,
+    },
+};
+
+type BroadcastInlet = MpscSender<Message>;
+
+pub struct Client {
+    brokers: Arc<StdMutex<Vec<SocketAddr>>>,
+    broadcast_inlet: TokioMutex<BroadcastInlet>,
+    _fuse: Fuse,
+}
+
+#[derive(Doom)]
+pub enum ClientError {
+    #[doom(description("Failed to resolve host: {:?}", source))]
+    #[doom(wrap(resolve_failed))]
+    ResolveFailed { source: io::Error },
+    #[doom(description("Host unknown"))]
+    HostUnknown,
+}
+
+impl Client {
+    pub fn new(id: u64, keychain: KeyChain) -> Self {
+        let brokers = Arc::new(StdMutex::new(Vec::new()));
+
+        let (broadcast_inlet, broadcast_outlet) = mpsc::channel(1); // Only one broadcast is performed at a time
+        let broadcast_inlet = TokioMutex::new(broadcast_inlet);
+
+        let fuse = Fuse::new();
+
+        {
+            let brokers = brokers.clone();
+            fuse.spawn(Client::run(id, keychain, brokers, broadcast_outlet));
+        }
+
+        Client {
+            brokers,
+            broadcast_inlet,
+            _fuse: fuse,
+        }
+    }
+
+    pub async fn add_broker<A>(&self, broker: A) -> Result<(), Top<ClientError>>
+    where
+        A: ToSocketAddrs,
+    {
+        let mut addresses = net::lookup_host(broker)
+            .await
+            .map_err(ClientError::resolve_failed)
+            .map_err(ClientError::into_top)
+            .spot(here!())?;
+
+        let address = addresses
+            .next()
+            .ok_or(ClientError::HostUnknown.into_top())
+            .spot(here!())?;
+
+        self.brokers.lock().unwrap().push(address);
+
+        Ok(())
+    }
+
+    pub async fn broadcast(&self, message: Message) {
+        let broadcast_inlet = self.broadcast_inlet.lock().await;
+
+        // If the `Client` still exists, then `_fuse` has not
+        // dropped, `run` is still running and `broadcast_outlet`
+        // still exists
+        broadcast_inlet.send(message).await.unwrap();
+    }
+}
+
+mod run;
--- src/client/client/run.rs
+++ src/client/client/run.rs
@@ -1,8 +1,7 @@
 use crate::{
-    broadcast::Message, broker::Request, crypto::statements::Broadcast as BroadcastStatement,
+    broadcast::Message, broker::Request, client::Client,
+    crypto::statements::Broadcast as BroadcastStatement,
 };
-
-use doomstack::{here, Doom, ResultExt, Top};
 
 use std::{
     net::SocketAddr,
@@ -16,85 +15,12 @@ use talk::{
     sync::fuse::Fuse,
 };
 
-use tokio::{
-    io,
-    net::{self, ToSocketAddrs},
-    sync::{
-        mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender},
-        Mutex as TokioMutex,
-    },
-    time,
-};
+use tokio::{sync::mpsc::Receiver as MpscReceiver, time};
 
-type BroadcastInlet = MpscSender<Message>;
 type BroadcastOutlet = MpscReceiver<Message>;
 
-pub struct Client {
-    brokers: Arc<StdMutex<Vec<SocketAddr>>>,
-    broadcast_inlet: TokioMutex<BroadcastInlet>,
-    _fuse: Fuse,
-}
-
-#[derive(Doom)]
-pub enum ClientError {
-    #[doom(description("Failed to resolve host: {:?}", source))]
-    #[doom(wrap(resolve_failed))]
-    ResolveFailed { source: io::Error },
-    #[doom(description("Host unknown"))]
-    HostUnknown,
-}
-
 impl Client {
-    pub fn new(id: u64, keychain: KeyChain) -> Self {
-        let brokers = Arc::new(StdMutex::new(Vec::new()));
-
-        let (broadcast_inlet, broadcast_outlet) = mpsc::channel(1); // Only one broadcast is performed at a time
-        let broadcast_inlet = TokioMutex::new(broadcast_inlet);
-
-        let fuse = Fuse::new();
-
-        {
-            let brokers = brokers.clone();
-            fuse.spawn(Client::run(id, keychain, brokers, broadcast_outlet));
-        }
-
-        Client {
-            brokers,
-            broadcast_inlet,
-            _fuse: fuse,
-        }
-    }
-
-    pub async fn add_broker<A>(&self, broker: A) -> Result<(), Top<ClientError>>
-    where
-        A: ToSocketAddrs,
-    {
-        let mut addresses = net::lookup_host(broker)
-            .await
-            .map_err(ClientError::resolve_failed)
-            .map_err(ClientError::into_top)
-            .spot(here!())?;
-
-        let address = addresses
-            .next()
-            .ok_or(ClientError::HostUnknown.into_top())
-            .spot(here!())?;
-
-        self.brokers.lock().unwrap().push(address);
-
-        Ok(())
-    }
-
-    pub async fn broadcast(&self, message: Message) {
-        let broadcast_inlet = self.broadcast_inlet.lock().await;
-
-        // If the `Client` still exists, then `_fuse` has not
-        // dropped, `run` is still running and `broadcast_outlet`
-        // still exists
-        broadcast_inlet.send(message).await.unwrap();
-    }
-
-    async fn run(
+    pub(in crate::client::client) async fn run(
         id: u64,
         keychain: KeyChain,
         brokers: Arc<StdMutex<Vec<SocketAddr>>>,
