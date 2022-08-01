761d8662673d3552184d7db97fb5a6d8d6240b5e
Matteo Monti <matteo.monti@epfl.ch>
2022-08-01 09:51:54 +0000

    Wire `Client`'s broadcast channel

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,19 +1,39 @@
+use crate::broadcast::Message;
+
 use talk::{crypto::KeyChain, sync::fuse::Fuse};
 
+use tokio::sync::mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender};
+
+type BroadcastInlet = MpscSender<Message>;
+type BroadcastOutlet = MpscReceiver<Message>;
+
 pub struct Client {
+    broadcast_inlet: BroadcastInlet,
     _fuse: Fuse,
 }
 
 impl Client {
     pub fn new(id: u64, keychain: KeyChain) -> Self {
+        let (broadcast_inlet, broadcast_outlet) = mpsc::channel(1); // Only one broadcast is performed at a time
+
         let fuse = Fuse::new();
 
         fuse.spawn(async move {
-            let _ = Client::run(id, keychain).await;
+            let _ = Client::run(id, keychain, broadcast_outlet).await;
         });
 
-        Client { _fuse: fuse }
+        Client {
+            broadcast_inlet,
+            _fuse: fuse,
+        }
     }
 
-    async fn run(_id: u64, _keychain: KeyChain) {}
+    pub async fn broadcast(&mut self, message: Message) {
+        // If the `Client` still exists, then `_fuse` has not
+        // dropped, `run` is still running and `broadcast_outlet`
+        // still exists
+        self.broadcast_inlet.send(message).await.unwrap();
+    }
+
+    async fn run(_id: u64, _keychain: KeyChain, _broadcast_outlet: BroadcastOutlet) {}
 }
