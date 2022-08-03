73d07bb72ed5e14562108cb4e4975fcdafc02485
Matteo Monti <matteo.monti@epfl.ch>
2022-08-03 09:45:06 +0000

    Scaffold `Reduction` statement

---
+++ src/crypto/statements/reduction.rs
@@ -0,0 +1,15 @@
+use crate::crypto::Header;
+
+use serde::Serialize;
+
+use talk::crypto::{primitives::hash::Hash, Statement};
+
+#[derive(Serialize)]
+pub(crate) struct Reduction {
+    pub root: Hash,
+}
+
+impl Statement for Reduction {
+    type Header = Header;
+    const HEADER: Self::Header = Header::BatchDelivery;
+}
