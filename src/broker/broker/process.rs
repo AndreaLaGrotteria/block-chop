962b3abde6e460e188b8502b9b6a915abede8105
Matteo Monti <matteo.monti@epfl.ch>
2022-08-04 13:04:11 +0000

    Begin scaffolding `Broker::process`

--- src/broker/broker/process.rs
+++ src/broker/broker/process.rs
@@ -12,9 +12,48 @@ type RequestInlet = MpscSender<(SocketAddr, Request)>;
 
 impl Broker {
     pub(in crate::broker::broker) async fn process(
-        _directory: Arc<Directory>,
-        _datagram_outlet: DatagramOutlet,
-        _request_inlet: RequestInlet,
+        directory: Arc<Directory>,
+        mut datagram_outlet: DatagramOutlet,
+        request_inlet: RequestInlet,
     ) {
+        loop {
+            // Get next `Request`
+
+            let (source, request) = if let Some(datagram) = datagram_outlet.recv().await {
+                datagram
+            } else {
+                // `Broker` has dropped, shutdown
+                return;
+            };
+
+            // Deserialize `request`
+
+            let request = if let Ok(request) = bincode::deserialize(request.as_slice()) {
+                request
+            } else {
+                continue;
+            };
+
+            // Verify `request`'s signatures
+
+            match &request {
+                Request::Broadcast {
+                    entry,
+                    signature,
+                    height_record,
+                } => {}
+                Request::Reduction {
+                    root,
+                    id,
+                    multisignature,
+                    authentication,
+                } => {}
+            }
+
+            // Send `request` over to `handle` task
+
+            // This fails only if the `Broker` is shutting down
+            let _ = request_inlet.send((source, request)).await;
+        }
     }
 }
