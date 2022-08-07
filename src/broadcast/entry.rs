162ba544449110092784aa274e38b7b9119f7745
Matteo Monti <matteo.monti@epfl.ch>
2022-08-07 16:42:15 +0000

    Derive `Debug` for `Entry`

--- src/broadcast/entry.rs
+++ src/broadcast/entry.rs
@@ -2,7 +2,7 @@ use crate::broadcast::Message;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Entry {
     pub id: u64,
     pub sequence: u64,
