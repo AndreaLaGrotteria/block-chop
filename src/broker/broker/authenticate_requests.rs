7e8050c29015777f22605011a461d7a5f5ae51f8
Matteo Monti <matteo.monti@epfl.ch>
2022-08-11 10:20:03 +0000

    Untested implementation of batched authentication

--- src/broker/broker/authenticate_requests.rs
+++ src/broker/broker/authenticate_requests.rs
@@ -12,10 +12,18 @@ use doomstack::{here, Doom, ResultExt, Top};
 
 use std::{net::SocketAddr, sync::Arc};
 
+use talk::crypto::{primitives::sign::Signature, KeyCard};
+
 use tokio::sync::mpsc::{Receiver as MpscReceiver, Sender as MpscSender};
 
 type RequestInlet = MpscSender<(SocketAddr, Request)>;
-type RequestOutlet = MpscReceiver<(SocketAddr, Request)>;
+type BurstOutlet = MpscReceiver<Vec<(SocketAddr, Request)>>;
+
+struct BurstItem<'a> {
+    source: SocketAddr,
+    request: Request,
+    keycard: &'a KeyCard,
+}
 
 #[derive(Doom)]
 enum FilterError {
@@ -32,34 +40,50 @@ enum FilterError {
 impl Broker {
     pub(in crate::broker::broker) async fn authenticate_requests(
         directory: Arc<Directory>,
-        mut authenticate_outlet: RequestOutlet,
+        mut authenticate_outlet: BurstOutlet,
         handle_inlet: RequestInlet,
     ) {
         loop {
-            let (source, request) = if let Some(datagram) = authenticate_outlet.recv().await {
-                datagram
+            let burst = if let Some(burst) = authenticate_outlet.recv().await {
+                burst
             } else {
                 // `Broker` has dropped, shutdown
                 return;
             };
 
-            if let Ok(request) = Broker::filter_request(directory.as_ref(), request) {
-                // This fails only if the `Broker` is shutting down
-                let _ = handle_inlet.send((source, request)).await;
+            let burst = burst
+                .into_iter()
+                .filter_map(|(source, request)| {
+                    Broker::filter_fields(directory.as_ref(), source, request).ok()
+                })
+                .collect::<Vec<_>>();
+
+            let burst = if Broker::batch_authenticate(burst.iter()).is_ok() {
+                burst
+            } else {
+                burst
+                    .into_iter()
+                    .filter_map(|item| Broker::filter_authentication(item).ok())
+                    .collect::<Vec<_>>()
+            };
+
+            for item in burst {
+                let _ = handle_inlet.send((item.source, item.request)).await;
             }
         }
     }
 
-    fn filter_request(
-        directory: &Directory,
+    fn filter_fields<'a>(
+        directory: &'a Directory,
+        source: SocketAddr,
         request: Request,
-    ) -> Result<Request, Top<FilterError>> {
-        match &request {
+    ) -> Result<BurstItem<'a>, Top<FilterError>> {
+        let keycard = match &request {
             Request::Broadcast {
                 entry,
-                signature,
                 height_record,
                 authentication,
+                ..
             } => {
                 // Fetch `KeyCard` from `directory`
 
@@ -68,38 +92,15 @@ impl Broker {
                     .ok_or(FilterError::IdUnknown.into_top())
                     .spot(here!())?;
 
-                // Verify `signature`
+                // Verify that `height_record` is justified
 
-                let broadcast_statement = BroadcastStatement {
-                    sequence: &entry.sequence,
-                    message: &entry.message,
-                };
-
-                signature
-                    .verify(keycard, &broadcast_statement)
-                    .pot(FilterError::InvalidSignature, here!())?;
-
-                // Verify `authentication`
-
-                if let Some(height_record) = height_record {
-                    let authentication = authentication
-                        .ok_or(FilterError::MissingAuthentication.into_top())
-                        .spot(here!())?;
-
-                    let authentication_statement =
-                        BroadcastAuthenticationStatement { height_record };
-
-                    authentication
-                        .verify(keycard, &authentication_statement)
-                        .pot(FilterError::InvalidAuthentication, here!())?;
+                if height_record.is_some() && authentication.is_none() {
+                    return FilterError::MissingAuthentication.fail().spot(here!());
                 }
+
+                keycard
             }
-            Request::Reduction {
-                root,
-                id,
-                multisignature,
-                authentication,
-            } => {
+            Request::Reduction { id, .. } => {
                 // Fetch `KeyCard` from `directory`
 
                 let keycard = directory
@@ -107,19 +108,148 @@ impl Broker {
                     .ok_or(FilterError::IdUnknown.into_top())
                     .spot(here!())?;
 
-                // Verify `authentication`
+                keycard
+            }
+        };
 
-                let authentication_statement = ReductionAuthenticationStatement {
+        Ok(BurstItem {
+            source,
+            request,
+            keycard,
+        })
+    }
+
+    fn batch_authenticate<'a, I>(burst: I) -> Result<(), Top<FilterError>>
+    where
+        I: IntoIterator<Item = &'a BurstItem<'a>>,
+    {
+        let mut broadcast_keycards = Vec::new();
+        let mut broadcast_statements = Vec::new();
+        let mut broadcast_signatures = Vec::new();
+
+        let mut broadcast_authentication_keycards = Vec::new();
+        let mut broadcast_authentication_statements = Vec::new();
+        let mut broadcast_authentication_signatures = Vec::new();
+
+        let mut reduction_authentication_keycards = Vec::new();
+        let mut reduction_authentication_statements = Vec::new();
+        let mut reduction_authentication_signatures = Vec::new();
+
+        for item in burst {
+            match &item.request {
+                Request::Broadcast {
+                    entry,
+                    signature,
+                    height_record,
+                    authentication,
+                    ..
+                } => {
+                    broadcast_keycards.push(item.keycard);
+
+                    broadcast_statements.push(BroadcastStatement {
+                        sequence: &entry.sequence,
+                        message: &entry.message,
+                    });
+
+                    broadcast_signatures.push(signature);
+
+                    if let Some(height_record) = height_record {
+                        broadcast_authentication_keycards.push(item.keycard);
+
+                        broadcast_authentication_statements
+                            .push(BroadcastAuthenticationStatement { height_record });
+
+                        broadcast_authentication_signatures.push(authentication.as_ref().unwrap());
+                    }
+                }
+                Request::Reduction {
+                    root,
+                    multisignature,
+                    authentication,
+                    ..
+                } => {
+                    reduction_authentication_keycards.push(item.keycard);
+
+                    reduction_authentication_statements.push(ReductionAuthenticationStatement {
+                        root,
+                        multisignature,
+                    });
+
+                    reduction_authentication_signatures.push(authentication);
+                }
+            }
+        }
+
+        Signature::batch_verify(
+            broadcast_keycards,
+            broadcast_statements.iter(),
+            broadcast_signatures,
+        )
+        .pot(FilterError::InvalidSignature, here!())?;
+
+        Signature::batch_verify(
+            broadcast_authentication_keycards,
+            broadcast_authentication_statements.iter(),
+            broadcast_authentication_signatures,
+        )
+        .pot(FilterError::InvalidAuthentication, here!())?;
+
+        Signature::batch_verify(
+            reduction_authentication_keycards,
+            reduction_authentication_statements.iter(),
+            reduction_authentication_signatures,
+        )
+        .pot(FilterError::InvalidAuthentication, here!())?;
+
+        Ok(())
+    }
+
+    fn filter_authentication(item: BurstItem) -> Result<BurstItem, Top<FilterError>> {
+        match &item.request {
+            Request::Broadcast {
+                entry,
+                signature,
+                height_record,
+                authentication,
+                ..
+            } => {
+                let broadcast_statement = BroadcastStatement {
+                    sequence: &entry.sequence,
+                    message: &entry.message,
+                };
+
+                signature
+                    .verify(item.keycard, &broadcast_statement)
+                    .pot(FilterError::InvalidSignature, here!())?;
+
+                if let Some(height_record) = height_record {
+                    let broadcast_authentication_statement =
+                        BroadcastAuthenticationStatement { height_record };
+
+                    authentication
+                        .as_ref()
+                        .unwrap()
+                        .verify(item.keycard, &broadcast_authentication_statement)
+                        .pot(FilterError::InvalidAuthentication, here!())?;
+                }
+            }
+            Request::Reduction {
+                root,
+                multisignature,
+                authentication,
+                ..
+            } => {
+                let reduction_authentication_statement = ReductionAuthenticationStatement {
                     root,
                     multisignature,
                 };
 
                 authentication
-                    .verify(keycard, &authentication_statement)
+                    .verify(item.keycard, &reduction_authentication_statement)
                     .pot(FilterError::InvalidAuthentication, here!())?;
             }
         }
 
-        Ok(request)
+        Ok(item)
     }
 }
--- src/broker/broker/dispatch_requests.rs
+++ src/broker/broker/dispatch_requests.rs
@@ -1,31 +1,47 @@
 use crate::broker::{Broker, Request};
 
-use std::net::SocketAddr;
+use std::{
+    mem,
+    net::SocketAddr,
+    time::{Duration, Instant},
+};
 
 use talk::net::DatagramReceiver;
 
-use tokio::sync::mpsc::Sender as MpscSender;
+use tokio::{sync::mpsc::Sender as MpscSender, time};
 
-type RequestInlet = MpscSender<(SocketAddr, Request)>;
+type BurstInlet = MpscSender<Vec<(SocketAddr, Request)>>;
 
 impl Broker {
     pub(in crate::broker::broker) async fn dispatch_requests(
         mut receiver: DatagramReceiver<Request>,
-        authenticate_inlets: Vec<RequestInlet>,
+        authenticate_inlets: Vec<BurstInlet>,
     ) {
         let mut robin = 0;
 
+        let mut burst_buffer = Vec::new();
+        let mut last_burst = Instant::now();
+
         loop {
-            let datagram = receiver.receive().await;
+            if let Some(datagram) = tokio::select! {
+                datagram = receiver.receive() => Some(datagram),
+                _ = time::sleep(Duration::from_millis(10)) => None
+            } {
+                burst_buffer.push(datagram);
+            }
 
-            // This fails only if the `Broker` is shutting down
-            let _ = authenticate_inlets
-                .get(robin % authenticate_inlets.len())
-                .unwrap()
-                .send(datagram)
-                .await;
+            // TODO: Add settings
+            if burst_buffer.len() >= 2000 || last_burst.elapsed() >= Duration::from_millis(100) {
+                // This fails only if the `Broker` is shutting down
+                let _ = authenticate_inlets
+                    .get(robin % authenticate_inlets.len())
+                    .unwrap()
+                    .send(mem::take(&mut burst_buffer))
+                    .await;
 
-            robin += 1;
+                robin += 1;
+                last_burst = Instant::now();
+            }
         }
     }
 }
