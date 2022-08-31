7c358853ecbbdfc0158a75596426cc0431a24a71
Manuel <mvidigueira@gmail.com>
2022-08-31 15:02:54 +0200

    Derive `serde::...` for `Straggler` and `CompressedBatch`

--- src/broadcast/compressed_batch.rs
+++ src/broadcast/compressed_batch.rs
@@ -1,9 +1,12 @@
 use crate::broadcast::{Message, Straggler};
 
+use serde::{Deserialize, Serialize};
+
 use talk::crypto::primitives::multi::Signature as MultiSignature;
 
 use varcram::VarCram;
 
+#[derive(Serialize, Deserialize)]
 pub(crate) struct CompressedBatch {
     pub ids: VarCram,
     pub messages: Vec<Message>,
--- src/broadcast/straggler.rs
+++ src/broadcast/straggler.rs
@@ -1,5 +1,8 @@
+use serde::{Deserialize, Serialize};
+
 use talk::crypto::primitives::sign::Signature;
 
+#[derive(Serialize, Deserialize)]
 pub(crate) struct Straggler {
     pub id: u64,
     pub sequence: u64,
