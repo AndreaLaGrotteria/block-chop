df674528517f5f933f0444a052cefc6155fd0725
Manuel <mvidigueira@gmail.com>
2022-09-02 13:38:46 +0200

    Add placeholder `Deduplicator::new` method

--- src/server/deduplicator.rs
+++ src/server/deduplicator.rs
@@ -5,7 +5,7 @@ use crate::{
 
 use talk::sync::fuse::Fuse;
 
-use tokio::sync::mpsc::{Receiver as MpscReceiver, Sender as MpscSender};
+use tokio::sync::mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender};
 
 type BatchInlet = MpscSender<Batch>;
 type AmendedBatchOutlet = MpscReceiver<(Batch, Vec<Amendment>)>;
@@ -22,4 +22,16 @@ struct Log {
     last_message: Message,
 }
 
-impl Deduplicator {}
+impl Deduplicator {
+    pub fn new() -> Self {
+        let (dispatch_inlet, _) = mpsc::channel(1);
+        let (_, pop_outlet) = mpsc::channel(1);
+        let _fuse = Fuse::new();
+
+        Self {
+            dispatch_inlet,
+            pop_outlet,
+            _fuse,
+        }
+    }
+}
