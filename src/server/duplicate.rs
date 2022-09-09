95b8cf8f3152e146b9c43ffd87b88a09e82a501a
Matteo Monti <matteo.monti@epfl.ch>
2022-09-09 13:24:16 +0000

    Implement `Duplicate::amendment`

--- src/server/duplicate.rs
+++ src/server/duplicate.rs
@@ -1,6 +1,18 @@
+use crate::broadcast::Amendment;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(in crate::server) enum Duplicate {
     Ignore { id: u64 },
     Nudge { id: u64, sequence: u64 },
     Drop { id: u64 },
+}
+
+impl Duplicate {
+    pub fn amendment(&self) -> Option<Amendment> {
+        match *self {
+            Duplicate::Ignore { .. } => None,
+            Duplicate::Nudge { id, sequence } => Some(Amendment::Nudge { id, sequence }),
+            Duplicate::Drop { id } => Some(Amendment::Drop { id }),
+        }
+    }
 }
