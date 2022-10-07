6ae523fc041bb8c3a558c5d044a94c2844711d23
Matteo Monti <matteo.monti@epfl.ch>
2022-10-07 14:03:40 +0000

    Refactor `Processor` to use static tasks

--- src/applications/payments/processor.rs
+++ src/applications/payments/processor.rs
@@ -9,16 +9,35 @@ use std::{
 };
 use talk::sync::fuse::Fuse;
 use tokio::{
-    sync::mpsc::{self, Receiver, Sender},
+    sync::{
+        mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender},
+        oneshot::{self, Sender as OneShotSender},
+    },
     task,
 };
 
-type BatchInlet = Sender<Vec<Payment>>;
-type BatchOutlet = Receiver<Vec<Payment>>;
+type BatchInlet = MpscSender<Vec<Payment>>;
+type BatchOutlet = MpscReceiver<Vec<Payment>>;
+
+type TaskOutlet = MpscReceiver<Task>;
+
+type DepositRowInlet = OneShotSender<Vec<Vec<Deposit>>>;
+type EmptyInlet = OneShotSender<()>;
 
 pub struct Processor {
     process_inlet: BatchInlet,
     _fuse: Fuse,
+}
+
+enum Task {
+    Withdraw {
+        batch_burst: Arc<Vec<Vec<Payment>>>,
+        return_inlet: DepositRowInlet,
+    },
+    Deposit {
+        deposit_column: Vec<Vec<Deposit>>,
+        return_inlet: EmptyInlet,
+    },
 }
 
 impl Processor {
@@ -60,13 +79,31 @@ impl Processor {
         mut process_outlet: BatchOutlet,
         settings: ProcessorSettings,
     ) {
-        // Initialize balance shards
+        // Spawn shard tasks
 
         // Each shard is relevant to `ceil(accounts / settings.shards)` accounts
         // (TODO: replace with `div_ceil` when `int_roundings` is stabilized)
         let shard_span = (accounts + (settings.shards as u64) - 1) / (settings.shards as u64);
 
-        let mut balance_shards = vec![vec![initial_balance; shard_span as usize]; settings.shards];
+        let task_inlets = (0..settings.shards)
+            .map(|shard| {
+                let settings = settings.clone();
+
+                let (task_inlet, task_outlet) = mpsc::channel(1);
+
+                task::spawn_blocking(move || {
+                    Processor::shard(
+                        shard as u64,
+                        shard_span,
+                        initial_balance,
+                        task_outlet,
+                        settings,
+                    );
+                });
+
+                task_inlet
+            })
+            .collect::<Vec<_>>();
 
         loop {
             // Collect next batch burst
@@ -84,87 +121,121 @@ impl Processor {
                 batch_burst.push(batch);
             }
 
-            // Apply `batch_burst` to `balance_shards`
-
             let batch_burst = Arc::new(batch_burst);
 
-            balance_shards = {
-                // Spawn and join withdraw tasks
+            // Perform withdraws to obtain deposit rows
 
-                let withdraw_tasks = balance_shards
-                    .into_iter()
-                    .enumerate()
-                    .map(|(shard, balance_shard)| {
-                        let batch_burst = batch_burst.clone();
-                        let settings = settings.clone();
+            let return_outlets = task_inlets
+                .iter()
+                .map(|task_inlet| {
+                    let (return_inlet, return_outlet) = oneshot::channel();
 
-                        task::spawn_blocking(move || {
-                            Processor::withdraw(
-                                batch_burst,
-                                shard as u64,
-                                shard_span,
-                                balance_shard,
-                                settings,
-                            )
-                        })
-                    })
-                    .collect::<FuturesOrdered<_>>();
+                    let task = Task::Withdraw {
+                        batch_burst: batch_burst.clone(),
+                        return_inlet,
+                    };
 
-                let withdraw_results = withdraw_tasks
-                    .map(|task| task.unwrap())
-                    .collect::<Vec<_>>()
-                    .await;
+                    // No more than one task is ever fed to `task_inlet` at the same time,
+                    // and `shard` does not shutdown before `task_inlet` is dropped
+                    let _ = task_inlet.try_send(task);
 
-                // Rebuild balance shards, transpose deposit matrix
+                    return_outlet
+                })
+                .collect::<FuturesOrdered<_>>();
 
-                let mut balance_shards = Vec::with_capacity(settings.shards);
+            let deposit_rows = return_outlets
+                .map(|return_outlet| return_outlet.unwrap())
+                .collect::<Vec<_>>()
+                .await;
 
-                let mut deposit_columns =
-                    vec![Vec::with_capacity(settings.shards); settings.shards];
+            // Transpose deposit matrix
 
-                for (balance_shard, deposit_row) in withdraw_results {
-                    balance_shards.push(balance_shard);
+            let mut deposit_columns = vec![Vec::with_capacity(settings.shards); settings.shards];
 
-                    for (deposit_column, bucket) in deposit_columns.iter_mut().zip(deposit_row) {
-                        deposit_column.push(bucket);
-                    }
+            for deposit_row in deposit_rows {
+                for (deposit_column, bucket) in deposit_columns.iter_mut().zip(deposit_row) {
+                    deposit_column.push(bucket);
                 }
+            }
 
-                // Spawn and join deposit tasks
+            // Perform deposits on deposit columns
 
-                let deposit_tasks = deposit_columns
-                    .into_iter()
-                    .zip(balance_shards)
-                    .enumerate()
-                    .map(|(shard, (deposit_column, balance_shard))| {
-                        task::spawn_blocking(move || {
-                            Processor::deposit(
-                                deposit_column,
-                                shard as u64,
-                                shard_span,
-                                balance_shard,
-                            )
-                        })
-                    })
-                    .collect::<FuturesOrdered<_>>();
+            let return_outlets = task_inlets
+                .iter()
+                .zip(deposit_columns)
+                .map(|(task_inlet, deposit_column)| {
+                    let (return_inlet, return_outlet) = oneshot::channel();
 
-                let balance_shards = deposit_tasks
-                    .map(|task| task.unwrap())
-                    .collect::<Vec<_>>()
-                    .await;
+                    let task = Task::Deposit {
+                        deposit_column,
+                        return_inlet,
+                    };
 
-                balance_shards
+                    // No more than one task is ever fed to `task_inlet` at the same time,
+                    // and `shard` does not shutdown before `task_inlet` is dropped
+                    let _ = task_inlet.try_send(task);
+
+                    return_outlet
+                })
+                .collect::<FuturesOrdered<_>>();
+
+            return_outlets
+                .map(|return_outlet| return_outlet.unwrap())
+                .collect::<()>()
+                .await;
+        }
+    }
+
+    fn shard(
+        shard: u64,
+        shard_span: u64,
+        initial_balance: u64,
+        mut task_outlet: TaskOutlet,
+        settings: ProcessorSettings,
+    ) {
+        let mut balance_shard = vec![initial_balance; shard_span as usize];
+
+        loop {
+            let task = if let Some(task) = task_outlet.blocking_recv() {
+                task
+            } else {
+                // `Processor` has dropped, shutdown
+                return;
             };
+
+            match task {
+                Task::Withdraw {
+                    batch_burst,
+                    return_inlet,
+                } => {
+                    let deposit_row = Processor::withdraw(
+                        batch_burst.as_ref(),
+                        shard,
+                        shard_span,
+                        &mut balance_shard,
+                        &settings,
+                    );
+
+                    let _ = return_inlet.send(deposit_row);
+                }
+                Task::Deposit {
+                    deposit_column,
+                    return_inlet,
+                } => {
+                    Processor::deposit(deposit_column, shard, shard_span, &mut balance_shard);
+                    let _ = return_inlet.send(());
+                }
+            }
         }
     }
 
     fn withdraw(
-        batch_burst: Arc<Vec<Vec<Payment>>>,
+        batch_burst: &Vec<Vec<Payment>>,
         shard: u64,
         shard_span: u64,
-        mut balance_shard: Vec<u64>,
-        settings: ProcessorSettings,
-    ) -> (Vec<u64>, Vec<Vec<Deposit>>) {
+        balance_shard: &mut Vec<u64>,
+        settings: &ProcessorSettings,
+    ) -> Vec<Vec<Deposit>> {
         let offset = shard * shard_span;
 
         let mut deposit_row =
@@ -185,15 +256,15 @@ impl Processor {
             }
         }
 
-        (balance_shard, deposit_row)
+        deposit_row
     }
 
     fn deposit(
         deposit_column: Vec<Vec<Deposit>>,
         shard: u64,
         shard_span: u64,
-        mut balance_shard: Vec<u64>,
-    ) -> Vec<u64> {
+        balance_shard: &mut Vec<u64>,
+    ) {
         let offset = shard * shard_span;
 
         for bucket in deposit_column {
@@ -201,8 +272,6 @@ impl Processor {
                 balance_shard[(deposit.to - offset) as usize] += deposit.amount;
             }
         }
-
-        balance_shard
     }
 
     fn crop<R>(payments: &[Payment], range: R) -> &[Payment]
