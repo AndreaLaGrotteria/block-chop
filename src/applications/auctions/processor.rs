fddab885e5e72ff647a71103236c5c091eeec51d
Matteo Monti <matteo.monti@epfl.ch>
2022-10-10 16:15:50 +0000

    Implement `Processor`
    Co-authored-by: Manuel Vidigueira <mvidigueira@users.noreply.github.com>

--- src/applications/auctions/processor.rs
+++ src/applications/auctions/processor.rs
@@ -1,11 +1,13 @@
-use crate::applications::auctions::Request;
-use std::sync::{atomic::AtomicU64, Arc};
-use talk::sync::fuse::Fuse;
+use crate::{
+    applications::auctions::{Bid, ProcessorSettings, Request, Token},
+    broadcast::Entry,
+};
+use std::sync::{
+    atomic::{AtomicU64, Ordering},
+    Arc,
+};
 use tokio::{
-    sync::{
-        mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender},
-        oneshot::{self, Sender as OneShotSender},
-    },
+    sync::mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender},
     task::{self},
 };
 
@@ -15,7 +17,137 @@ type BatchOutlet = MpscReceiver<Vec<Request>>;
 pub struct Processor {
     process_inlet: BatchInlet,
     operations_processed: Arc<AtomicU64>,
-    _fuse: Fuse,
 }
 
-impl Processor {}
+impl Processor {
+    pub fn new(
+        accounts: u64,
+        initial_balance: u64,
+        token_owners: Vec<u64>,
+        settings: ProcessorSettings,
+    ) -> Self {
+        let (process_inlet, process_outlet) = mpsc::channel(settings.process_channel_capacity);
+        let operations_processed = Arc::new(AtomicU64::new(0));
+
+        {
+            let operations_processed = operations_processed.clone();
+
+            task::spawn_blocking(move || {
+                Processor::process(
+                    accounts,
+                    initial_balance,
+                    token_owners,
+                    process_outlet,
+                    operations_processed,
+                )
+            });
+        }
+
+        Processor {
+            process_inlet,
+            operations_processed,
+        }
+    }
+
+    pub fn operations_processed(&self) -> u64 {
+        self.operations_processed.load(Ordering::Relaxed)
+    }
+
+    pub async fn push<I>(&self, batch: I)
+    where
+        I: IntoIterator<Item = Entry>,
+    {
+        let batch = batch
+            .into_iter()
+            .map(Request::from_entry)
+            .collect::<Vec<_>>();
+
+        // A copy of `process_outlet` is held by `Processor::process`,
+        // whose `Fuse` is held by `self`, so `process_inlet.send()`
+        // cannot return an `Err`.
+        self.process_inlet.send(batch).await.unwrap();
+    }
+
+    fn process(
+        accounts: u64,
+        initial_balance: u64,
+        token_owners: Vec<u64>,
+        mut process_outlet: BatchOutlet,
+        operations_processed: Arc<AtomicU64>,
+    ) {
+        let mut balances = vec![initial_balance; accounts as usize];
+
+        let mut tokens = token_owners
+            .into_iter()
+            .map(|owner| Token {
+                owner,
+                best_bid: None,
+            })
+            .collect::<Vec<_>>();
+
+        loop {
+            let batch = if let Some(batch) = process_outlet.blocking_recv() {
+                batch
+            } else {
+                // `Processor` dropped, shutdown
+                return;
+            };
+
+            let batch_operation_count = batch.len() as u64;
+
+            for request in batch {
+                match request {
+                    Request::Bid {
+                        bidder,
+                        token,
+                        offer,
+                    } => {
+                        let bidder_balance = balances.get_mut(bidder as usize).unwrap();
+
+                        if *bidder_balance >= offer {
+                            *bidder_balance -= offer;
+                        } else {
+                            continue;
+                        }
+
+                        let bid = Bid { bidder, offer };
+
+                        let token = if let Some(token) = tokens.get_mut(token as usize) {
+                            token
+                        } else {
+                            continue;
+                        };
+
+                        let refund_bid = if bid.outbids(&token.best_bid) {
+                            token.best_bid.replace(bid)
+                        } else {
+                            Some(bid)
+                        };
+
+                        if let Some(refund_bid) = refund_bid {
+                            balances[refund_bid.bidder as usize] += refund_bid.offer;
+                        }
+                    }
+                    Request::Take { taker, token } => {
+                        let token = if let Some(token) = tokens.get_mut(token as usize) {
+                            token
+                        } else {
+                            continue;
+                        };
+
+                        if taker != token.owner {
+                            continue;
+                        }
+
+                        if let Some(best_bid) = token.best_bid.take() {
+                            balances[token.owner as usize] += best_bid.offer;
+                            token.owner = best_bid.bidder;
+                        }
+                    }
+                }
+            }
+
+            operations_processed.fetch_add(batch_operation_count, Ordering::Relaxed);
+        }
+    }
+}
