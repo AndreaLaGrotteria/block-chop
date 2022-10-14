3b200feff8f3626fd3bb34992cd962987996e7df
Matteo Monti <matteo.monti@epfl.ch>
2022-10-14 12:59:07 +0000

    Implement `rendezvous`

---
+++ bin/rendezvous.rs
@@ -0,0 +1,35 @@
+use std::{future, io, io::Write};
+use talk::link::rendezvous::{Server, ServerSettings};
+
+#[tokio::main]
+async fn main() {
+    let args = lapp::parse_args(
+        "
+        Welcome to `chop-chop`'s `Rendezvous` server.
+
+        Required arguments:
+          <port> (integer) port to which to bind the `Rendezvous` server
+          <expected_participants> (integer) number of `Server`s and `Broker`s to wait for
+        ",
+    );
+
+    let port = args.get_integer("port") as u16;
+    let expected_participants = args.get_integer("port") as usize;
+
+    println!("Starting `Rendezvous` server..");
+
+    let _server = Server::new(
+        ("0.0.0.0", port),
+        ServerSettings {
+            shard_sizes: vec![expected_participants],
+        },
+    )
+    .await
+    .unwrap();
+
+    println!(" .. done!");
+
+    print!("\n    [Hit Ctrl + C to stop this daemon]  ");
+    io::stdout().flush().unwrap();
+    future::pending::<()>().await;
+}
