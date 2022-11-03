27b053f6ba539a18ed0fda87bc372f0b00298e69
Matteo Monti <matteo.monti@epfl.ch>
2022-11-04 00:12:28 +0100

    Begin scaffolding `heartbeat_statistics`

---
+++ bin/heartbeat_statistics.rs
@@ -0,0 +1,148 @@
+fn main() {
+    let args = lapp::parse_args(
+        "
+        Welcome to `chop-chop`'s `heartbeat` data statistics tool
+
+        Choose one of the following:
+          --shallow-broker (string) path to `Broker` / `LoadBroker` `heartbeat` data
+        ",
+    );
+
+    let shallow_broker = args.get_string_result("shallow-broker").ok();
+
+    if [&shallow_broker].into_iter().flatten().count() != 1 {
+        println!("Please select one of the available statistical modes.");
+        return;
+    }
+
+    if let Some(path) = shallow_broker {
+        modes::shallow_broker(path);
+    }
+}
+
+mod modes {
+    use chop_chop::heartbeat::{BrokerEvent, Entry, Event};
+    use rayon::slice::ParallelSliceMut;
+    use std::{collections::HashMap, fs::File, io::Read, time::SystemTime};
+    use talk::crypto::{primitives::hash::Hash, Identity};
+
+    struct BrokerSubmission {
+        submission_started: SystemTime,
+        server_connected: Option<SystemTime>,
+        batch_sent: Option<SystemTime>,
+        witness_shard_requested: Option<SystemTime>,
+        witness_shard_received: Option<SystemTime>,
+        witness_shard_verified: Option<SystemTime>,
+        witness_shard_waived: Option<SystemTime>,
+        witness_shard_concluded: Option<SystemTime>,
+        witness_acquired: Option<SystemTime>,
+        witness_sent: Option<SystemTime>,
+        delivery_shard_received: Option<SystemTime>,
+        submission_completed: Option<SystemTime>,
+    }
+
+    pub fn shallow_broker(path: String) {
+        // Load, deserialize and sort `Entry`ies by time
+
+        let mut file = File::open(path).unwrap();
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).unwrap();
+
+        let mut entries = bincode::deserialize::<Vec<Entry>>(buffer.as_slice()).unwrap();
+        entries.par_sort_unstable_by_key(|entry| entry.time);
+
+        // Initialize table of `BrokerSubmission`s
+
+        let mut submissions: HashMap<Identity, HashMap<Hash, Vec<BrokerSubmission>>> =
+            HashMap::new();
+
+        fn last_submission(
+            submissions: &mut HashMap<Identity, HashMap<Hash, Vec<BrokerSubmission>>>,
+            root: Hash,
+            server: Identity,
+        ) -> &mut BrokerSubmission {
+            submissions
+                .get_mut(&server)
+                .unwrap()
+                .get_mut(&root)
+                .unwrap()
+                .last_mut()
+                .unwrap()
+        }
+
+        // Loop through `entries` to fill `submissions`
+
+        for entry in entries {
+            let time = entry.time;
+
+            let broker_event = match entry.event {
+                Event::Broker(event) => event,
+                _ => unreachable!(),
+            };
+
+            match broker_event {
+                BrokerEvent::SubmissionStarted { root, server } => {
+                    submissions
+                        .entry(server)
+                        .or_default()
+                        .entry(root)
+                        .or_default()
+                        .push(BrokerSubmission {
+                            submission_started: time,
+                            server_connected: None,
+                            batch_sent: None,
+                            witness_shard_requested: None,
+                            witness_shard_received: None,
+                            witness_shard_verified: None,
+                            witness_shard_waived: None,
+                            witness_shard_concluded: None,
+                            witness_acquired: None,
+                            witness_sent: None,
+                            delivery_shard_received: None,
+                            submission_completed: None,
+                        });
+                }
+                BrokerEvent::ServerConnected { root, server } => {
+                    last_submission(&mut submissions, root, server).server_connected = Some(time);
+                }
+                BrokerEvent::BatchSent { root, server } => {
+                    last_submission(&mut submissions, root, server).batch_sent = Some(time);
+                }
+                BrokerEvent::WitnessShardRequested { root, server } => {
+                    last_submission(&mut submissions, root, server).witness_shard_requested =
+                        Some(time);
+                }
+                BrokerEvent::WitnessShardReceived { root, server } => {
+                    last_submission(&mut submissions, root, server).witness_shard_received =
+                        Some(time);
+                }
+                BrokerEvent::WitnessShardVerified { root, server } => {
+                    last_submission(&mut submissions, root, server).witness_shard_verified =
+                        Some(time);
+                }
+                BrokerEvent::WitnessShardWaived { root, server } => {
+                    last_submission(&mut submissions, root, server).witness_shard_waived =
+                        Some(time);
+                }
+                BrokerEvent::WitnessShardConcluded { root, server } => {
+                    last_submission(&mut submissions, root, server).witness_shard_concluded =
+                        Some(time);
+                }
+                BrokerEvent::WitnessAcquired { root, server } => {
+                    last_submission(&mut submissions, root, server).witness_acquired = Some(time);
+                }
+                BrokerEvent::WitnessSent { root, server } => {
+                    last_submission(&mut submissions, root, server).witness_sent = Some(time);
+                }
+                BrokerEvent::DeliveryShardReceived { root, server } => {
+                    last_submission(&mut submissions, root, server).delivery_shard_received =
+                        Some(time);
+                }
+                BrokerEvent::SubmissionCompleted { root, server } => {
+                    last_submission(&mut submissions, root, server).submission_completed =
+                        Some(time);
+                }
+            }
+        }
+    }
+}
