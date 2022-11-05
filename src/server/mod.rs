e8a33180cfa80e1cf327420b50b284120c63f64d
Matteo Monti <matteo.monti@epfl.ch>
2022-11-05 18:02:33 +0000

    Rename `batch` module to `merkle_batch`

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,8 +1,8 @@
-mod batch;
 mod broker_slot;
 mod deduplicator;
 mod deduplicator_settings;
 mod duplicate;
+mod merkle_batch;
 mod server;
 mod server_settings;
 mod totality_manager;
@@ -19,7 +19,7 @@ use totality_manager_settings::TotalityManagerSettings;
 use witness_cache::WitnessCache;
 use witness_cache_settings::WitnessCacheSettings;
 
-pub(crate) use batch::MerkleBatch;
+pub(crate) use merkle_batch::MerkleBatch;
 
 pub use server::Server;
 pub use server_settings::ServerSettings;
