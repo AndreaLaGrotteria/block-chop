640d65d10ea4d06974c803474ed26cabbc3ea5a7
Matteo Monti <matteo.monti@epfl.ch>
2022-11-08 17:24:37 +0000

    Implement `InflatedBatch`

--- src/server/batches/mod.rs
+++ src/server/batches/mod.rs
@@ -1,9 +1,11 @@
 mod compressed_batch;
+mod inflated_batch;
 mod merkle_batch;
 mod plain_batch;
 
 use compressed_batch::Delta;
 
 pub(crate) use compressed_batch::CompressedBatch;
+pub(crate) use inflated_batch::InflatedBatch;
 pub(crate) use merkle_batch::MerkleBatch;
 pub(crate) use plain_batch::PlainBatch;
