6a780ca5984793216c516598ee73815d21e2caf1
Matteo Monti <matteo.monti@epfl.ch>
2022-11-10 16:12:31 +0000

    Increase `lockstep_margin`

--- src/broker/load_broker_settings.rs
+++ src/broker/load_broker_settings.rs
@@ -22,7 +22,7 @@ impl Default for LoadBrokerSettings {
             warmup: Duration::from_secs(15),
             dissemination_delay: Duration::from_secs(3),
             lockstep_delta: 32,
-            lockstep_margin: Duration::from_millis(100),
+            lockstep_margin: Duration::from_secs(1),
             witnessing_timeout: Duration::from_secs(15),
             totality_timeout: Duration::from_secs(60),
             workers: 32768,
