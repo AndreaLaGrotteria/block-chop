4abe7967be527875074d5c4be31a926a28faf0ba
Matteo Monti <matteo.monti@epfl.ch>
2022-12-04 14:31:46 +0100

    Generalize `Message` serialization to any multiple of 8 bytes

--- src/broadcast/message.rs
+++ src/broadcast/message.rs
@@ -3,7 +3,7 @@ use rand::{
     distributions::{Distribution, Standard},
     Rng,
 };
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{de::SeqAccess, ser::SerializeTuple, Deserialize, Deserializer, Serialize, Serializer};
 use std::fmt::{self, Formatter};
 
 message!();
@@ -25,7 +25,13 @@ impl Serialize for Message {
     where
         S: Serializer,
     {
-        serializer.serialize_u64(u64::from_be_bytes(self.bytes))
+        let mut seq = serializer.serialize_tuple(MESSAGE_SIZE / 8)?;
+
+        for chunk in self.bytes.chunks(8) {
+            seq.serialize_element(&u64::from_be_bytes(chunk.try_into().unwrap()))?;
+        }
+
+        seq.end()
     }
 }
 
@@ -36,25 +42,33 @@ impl<'de> Deserialize<'de> for Message {
     {
         use serde::de::{Error, Visitor};
 
-        struct ByteVisitor;
+        struct BlockVisitor;
 
-        impl<'de> Visitor<'de> for ByteVisitor {
+        impl<'de> Visitor<'de> for BlockVisitor {
             type Value = Message;
 
             fn expecting(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
-                f.write_str("byte representation of a `Message`")
+                f.write_str("a `Message` encoded as a tuple of `u64`")
             }
 
-            fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
             where
-                E: Error,
+                A: SeqAccess<'de>,
             {
-                Ok(Message {
-                    bytes: v.to_be_bytes(),
-                })
+                let mut bytes = [0u8; MESSAGE_SIZE];
+
+                for (index, chunk) in bytes.chunks_mut(8).enumerate() {
+                    let block = seq
+                        .next_element::<u64>()?
+                        .ok_or_else(|| Error::invalid_length(index, &self))?;
+
+                    chunk.copy_from_slice(&block.to_be_bytes());
+                }
+
+                Ok(Message { bytes })
             }
         }
 
-        deserializer.deserialize_u64(ByteVisitor)
+        deserializer.deserialize_tuple(MESSAGE_SIZE / 8, BlockVisitor)
     }
 }
